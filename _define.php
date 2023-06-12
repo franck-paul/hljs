efa1bb7fb00f23ae835f96d7f3839e1f292d98ad
franck <carnet.franck.paul@gmail.com>
2023-06-12 10:23:59 +0200

    Don't do things in frontend if plugin disabled

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Code highlight',
     'highlight.js for Dotclear',
     'Franck Paul',
-    '2.0.2',
+    '2.0.3',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
