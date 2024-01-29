b37df5418be5bafd6c9878cb97e1269e2f01abca
franck <carnet.franck.paul@gmail.com>
2024-01-29 11:44:23 +0100

    New release 4.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Code highlight',
     'highlight.js for Dotclear',
     'Franck Paul',
-    '4.2',
+    '4.3',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
