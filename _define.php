97bae25c330f299e46ec8a250b4c48710ebd63b7
franck <carnet.franck.paul@gmail.com>
2025-03-05 01:14:37 +0100

    Plugins adding buttons on legacyEditor/CKEditor should have at least 1010 priority 1005 is reserved for additional syntaxes to legacyEditor (as legacyMarkdown)

--- _define.php
+++ _define.php
@@ -20,7 +20,7 @@ $this->registerModule(
         'date'        => '2025-02-08T15:10:04+0100',
         'requires'    => [['core', '2.29']],
         'permissions' => 'My',
-        'priority'    => 1001, // Must be higher than dcLegacyEditor priority (ie 1000)
+        'priority'    => 1010,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
         'type'        => 'plugin',
         'settings'    => [
             'self' => '',
@@ -29,5 +29,6 @@ $this->registerModule(
         'details'    => 'https://open-time.net/docs/plugins/hljs/',
         'support'    => 'https://github.com/franck-paul/hljs',
         'repository' => 'https://raw.githubusercontent.com/franck-paul/hljs/main/dcstore.xml',
+        'license'    => 'gpl2',
     ]
 );
