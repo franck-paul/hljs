40277900fdd6578acf4a99568e1266c94562f068
franck <carnet.franck.paul@gmail.com>
2026-02-13 10:45:51 +0100

    New release 8.0.1

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Code highlight',
     'highlight.js for Dotclear',
     'Franck Paul',
-    '8.0',
+    '8.0.1',
     [
-        'date'        => '2026-02-13T10:44:52+0100',
+        'date'        => '2026-02-13T10:45:51+0100',
         'requires'    => [['core', '2.37']],
         'permissions' => 'My',
         'priority'    => 1010,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
