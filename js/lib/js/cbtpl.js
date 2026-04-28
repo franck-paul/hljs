1a67a36cff533869513c8e64cbee8d62226266ff
franck <contact@open-time.net>
2026-04-28 14:11:35 +0200

    Update Franck's email

--- js/lib/js/cbtpl.js
+++ js/lib/js/cbtpl.js
@@ -3,74 +3,94 @@
 /*
 Language: cbtpl
 Requires: xml.js
-Author: Franck Paul <carnet.franck.paul@gmail.com>
+Author: Franck Paul <contact@open-time.net>
 Description: Clearbricks templates (used by Dotclear)
 Category: template
 */
 const hljsExtentCbtpl = (hljs) => {
-  const e = "[A-Za-z0-9\\._:-]+";
+  const e = '[A-Za-z0-9\\._:-]+';
   const t = {
     eW: !0,
     i: /</,
     r: 0,
-    c: [{
-      cN: "attr",
-      b: e,
-      r: 0
-    }, {
-      b: /=\s*/,
-      r: 0,
-      c: [{
-        cN: "string",
-        endsParent: !0,
-        v: [{
-          b: /"/,
-          e: /"/
-        }, {
-          b: /'/,
-          e: /'/
-        }, {
-          b: /[^\s"'=<>`]+/
-        }]
-      }]
-    }]
+    c: [
+      {
+        cN: 'attr',
+        b: e,
+        r: 0,
+      },
+      {
+        b: /=\s*/,
+        r: 0,
+        c: [
+          {
+            cN: 'string',
+            endsParent: !0,
+            v: [
+              {
+                b: /"/,
+                e: /"/,
+              },
+              {
+                b: /'/,
+                e: /'/,
+              },
+              {
+                b: /[^\s"'=<>`]+/,
+              },
+            ],
+          },
+        ],
+      },
+    ],
   };
   return {
     aliases: ['dctpl'],
     cI: false,
     sL: 'xml',
     c: [
-      hljs.C('<!-- #', '-->'), {
+      hljs.C('<!-- #', '-->'),
+      {
         cN: 'template-tag',
         b: /<tpl:?/,
         e: /\>/,
         r: 10,
-        c: [{
-          cN: "name",
-          b: /[^}}{{\/><\s]+/,
-          r: 0
-        }, t]
-      }, {
+        c: [
+          {
+            cN: 'name',
+            b: /[^}}{{\/><\s]+/,
+            r: 0,
+          },
+          t,
+        ],
+      },
+      {
         cN: 'template-tag',
         b: /<\/tpl:?/,
         e: /\>/,
         r: 10,
-        c: [{
-          cN: "name",
-          b: /[^}}{{\/><\s]+/,
-          r: 0
-        }]
-      }, {
+        c: [
+          {
+            cN: 'name',
+            b: /[^}}{{\/><\s]+/,
+            r: 0,
+          },
+        ],
+      },
+      {
         cN: 'template-variable',
         b: /{{tpl:?/,
         e: /}}/,
         r: 10,
-        c: [{
-          cN: "name",
-          b: /[^}}{{\/><\s]+/,
-          r: 0
-        }, t]
-      }
-    ]
+        c: [
+          {
+            cN: 'name',
+            b: /[^}}{{\/><\s]+/,
+            r: 0,
+          },
+          t,
+        ],
+      },
+    ],
   };
 };
