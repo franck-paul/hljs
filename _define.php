481738864e6c2a3c15ad3b845c3dc9e7949b269a
franck <contact@open-time.net>
2026-08-13 16:41:55 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,24 +11,28 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'Code highlight',
-    'highlight.js for Dotclear',
-    'Franck Paul',
-    '9.0',
-    [
-        'date'        => '2026-08-03T09:59:57+0200',
-        'requires'    => [['core', '2.39']],
-        'permissions' => 'My',
-        'priority'    => 1010,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
-        'type'        => 'plugin',
-        'settings'    => [
-            'self' => '',
-        ],
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/docs/plugins/hljs/',
-        'support'    => 'https://github.com/franck-paul/hljs',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/hljs/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'Code highlight',
+        'highlight.js for Dotclear',
+        'Franck Paul',
+        '9.0',
+        [
+            'date'        => '2026-08-03T09:59:57+0200',
+            'requires'    => [['core', '2.39']],
+            'permissions' => 'My',
+            'priority'    => 1010,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
+            'type'        => 'plugin',
+            'settings'    => [
+                'self' => '',
+            ],
+
+            'details'    => 'https://open-time.net/docs/plugins/hljs/',
+            'support'    => 'https://github.com/franck-paul/hljs',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/hljs/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
